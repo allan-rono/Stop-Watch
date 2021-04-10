e99596993d411305a87a3e99434e32dfc7497e25
Allan Rono <allanrono@hotmail.com>
2021-04-10 20:14:45 +1000

    Functionality Implemented

--- script.js
+++ script.js
@@ -1,12 +1,69 @@
 const display = document.getElementById("display");
 const start = document.getElementById("start");
-const split = document.getElementById("split");
+const stop = document.getElementById("stop");
 const reset = document.getElementById("reset");
 
-function startTime (){};
-function splitTime (){};
-function resetTime(){};
+
+var hours = 0;
+var minutes = 0;
+var seconds = 0;
+var timeStop = true;
+
+
+function startTime (){
+
+    if (timeStop == true){
+        timeStop = false;
+        timeCycle();   
+    }
+};
+
+function timeCycle(){
+    if (timeStop == false){
+        seconds = parseInt(seconds);
+        minutes = parseInt(minutes);
+        hours = parseInt(hours);
+
+        seconds++;
+
+        if(seconds ==60){
+            minutes++
+            seconds =0;
+        }
+        if (minutes ==60){
+            hours++
+            minutes=0
+            seconds=0
+        }
+        if(seconds <10|| seconds ==0){
+            seconds = "0"+seconds;
+        }
+        if (minutes < 10 || minutes == 0) {
+            minutes = "0" + minutes;
+        }
+        if (seconds < 10 || seconds == 0) {
+            minutes = "0" + minutes;
+        }
+
+        display.innerHTML = hours + ":"+minutes+":"+seconds;
+        setTimeout("timeCycle()",1000);
+    }
+}
+
+function stopTime (){
+    if(timeStop == false){
+        timeStop = true;
+    }
+};
+
+function resetTime(){
+    display.innerHTML = "00:00:00";
+    timeStop = true;
+    hours=0;
+    minutes=0;
+    seconds=0;
+};
 
 start.addEventListener("click",startTime);
-split.addEventListener("click", splitTime);
+stop.addEventListener("click", stopTime);
 reset.addEventListener("click", resetTime);
